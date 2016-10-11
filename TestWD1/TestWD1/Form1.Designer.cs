6d15d422b004a337aeec28ffb122ff1b1e498dcb
teinhonglo <teinhonglo@gmail.com>
2016-10-11 22:57:32 +0800

    Guassian

--- TestWD1/TestWD1/Form1.Designer.cs
+++ TestWD1/TestWD1/Form1.Designer.cs
@@ -29,55 +29,47 @@
         private void InitializeComponent()
         {
             this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
-            this.UploadBt = new System.Windows.Forms.Button();
             this.originImg = new System.Windows.Forms.PictureBox();
             this.procImg = new System.Windows.Forms.PictureBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.HistogramBT = new System.Windows.Forms.Button();
+            this.UploadBt = new System.Windows.Forms.Button();
+            this.Guassion_NoiseBT = new System.Windows.Forms.Button();
             this.tableLayoutPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.originImg)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.procImg)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel
             // 
             this.tableLayoutPanel.ColumnCount = 2;
-            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 49.30556F));
-            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.69444F));
-            this.tableLayoutPanel.Controls.Add(this.UploadBt, 0, 0);
-            this.tableLayoutPanel.Controls.Add(this.originImg, 0, 1);
-            this.tableLayoutPanel.Controls.Add(this.procImg, 1, 1);
-            this.tableLayoutPanel.Location = new System.Drawing.Point(3, 0);
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel.Controls.Add(this.originImg, 0, 0);
+            this.tableLayoutPanel.Controls.Add(this.procImg, 1, 0);
+            this.tableLayoutPanel.Location = new System.Drawing.Point(3, 84);
             this.tableLayoutPanel.Name = "tableLayoutPanel";
-            this.tableLayoutPanel.RowCount = 2;
-            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.6F));
-            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 87.4F));
-            this.tableLayoutPanel.Size = new System.Drawing.Size(576, 499);
+            this.tableLayoutPanel.RowCount = 1;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel.Size = new System.Drawing.Size(656, 328);
             this.tableLayoutPanel.TabIndex = 0;
-            // 
-            // UploadBt
-            // 
-            this.UploadBt.Location = new System.Drawing.Point(3, 3);
-            this.UploadBt.Name = "UploadBt";
-            this.UploadBt.Size = new System.Drawing.Size(75, 23);
-            this.UploadBt.TabIndex = 0;
-            this.UploadBt.Text = "Upload";
-            this.UploadBt.UseVisualStyleBackColor = true;
-            this.UploadBt.Click += new System.EventHandler(this.UploadBt_Click);
             // 
             // originImg
             // 
-            this.originImg.Location = new System.Drawing.Point(3, 65);
+            this.originImg.Location = new System.Drawing.Point(3, 3);
             this.originImg.Name = "originImg";
-            this.originImg.Size = new System.Drawing.Size(278, 300);
+            this.originImg.Size = new System.Drawing.Size(317, 318);
             this.originImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.originImg.TabIndex = 1;
             this.originImg.TabStop = false;
             // 
             // procImg
             // 
-            this.procImg.Location = new System.Drawing.Point(287, 65);
+            this.procImg.Location = new System.Drawing.Point(326, 3);
             this.procImg.Name = "procImg";
-            this.procImg.Size = new System.Drawing.Size(286, 300);
+            this.procImg.Size = new System.Drawing.Size(322, 318);
             this.procImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.procImg.TabIndex = 2;
             this.procImg.TabStop = false;
@@ -86,18 +78,68 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.HistogramBT, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.UploadBt, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.Guassion_NoiseBT, 2, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(6, 4);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(653, 74);
+            this.tableLayoutPanel1.TabIndex = 1;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // HistogramBT
+            // 
+            this.HistogramBT.Location = new System.Drawing.Point(166, 3);
+            this.HistogramBT.Name = "HistogramBT";
+            this.HistogramBT.Size = new System.Drawing.Size(75, 23);
+            this.HistogramBT.TabIndex = 5;
+            this.HistogramBT.Text = "Histogram";
+            this.HistogramBT.UseVisualStyleBackColor = true;
+            this.HistogramBT.Click += new System.EventHandler(this.HistogramBT_Click);
+            // 
+            // UploadBt
+            // 
+            this.UploadBt.Location = new System.Drawing.Point(3, 3);
+            this.UploadBt.Name = "UploadBt";
+            this.UploadBt.Size = new System.Drawing.Size(75, 23);
+            this.UploadBt.TabIndex = 4;
+            this.UploadBt.Text = "Upload";
+            this.UploadBt.UseVisualStyleBackColor = true;
+            this.UploadBt.Click += new System.EventHandler(this.UploadBt_Click);
+            // 
+            // Guassion_NoiseBT
+            // 
+            this.Guassion_NoiseBT.Location = new System.Drawing.Point(329, 3);
+            this.Guassion_NoiseBT.Name = "Guassion_NoiseBT";
+            this.Guassion_NoiseBT.Size = new System.Drawing.Size(75, 23);
+            this.Guassion_NoiseBT.TabIndex = 6;
+            this.Guassion_NoiseBT.Text = "Noise";
+            this.Guassion_NoiseBT.UseVisualStyleBackColor = true;
+            this.Guassion_NoiseBT.Click += new System.EventHandler(this.Guassion_NoiseBT_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.ClientSize = new System.Drawing.Size(577, 499);
+            this.ClientSize = new System.Drawing.Size(671, 499);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.tableLayoutPanel);
             this.Name = "Form1";
             this.Text = "AIP 60547047S";
             this.tableLayoutPanel.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.originImg)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.procImg)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -105,10 +147,13 @@
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
-        private System.Windows.Forms.Button UploadBt;
         private System.Windows.Forms.PictureBox originImg;
-        private System.Windows.Forms.PictureBox procImg;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox procImg;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button HistogramBT;
+        private System.Windows.Forms.Button UploadBt;
+        private System.Windows.Forms.Button Guassion_NoiseBT;
     }
 }
 
--- TestWD1/TestWD1/Form1.cs
+++ TestWD1/TestWD1/Form1.cs
@@ -8,42 +8,221 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Imaging;
 
 namespace TestWD1
 {
     public partial class Form1 : Form
     {
+        static string uploadPath = System.Environment.CurrentDirectory + "\\Origin\\";
+        static string savePath = System.Environment.CurrentDirectory + "\\Output\\";
+        static string readPath = System.Environment.CurrentDirectory + "\\Read\\";
         public Form1()
         {
             InitializeComponent();
         }
-
         private void UploadBt_Click(object sender, EventArgs e)
         {
             openFileDialog1.Filter = "";
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 string ImgPath = openFileDialog1.FileName;
-                string uploadPath = System.Environment.CurrentDirectory + "/Origin/";
-                string savePath = System.Environment.CurrentDirectory + "/Output/";
-                try 
+                try
                 {
-                    if (Directory.Exists(uploadPath) == false)  Directory.CreateDirectory(uploadPath);
-                    if(Directory.Exists(savePath) == false)     Directory.CreateDirectory(savePath);
-                    
-                    Bitmap input = new Bitmap(ImgPath);
-                    input.Save(uploadPath + "origin.png");
-                    input.Save(savePath + "output.png");
+                    if (Directory.Exists(uploadPath) == false) Directory.CreateDirectory(uploadPath);
+                    if (Directory.Exists(savePath) == false) Directory.CreateDirectory(savePath);
+                    if (Directory.Exists(readPath) == false) Directory.CreateDirectory(readPath);
 
+                    MemoryStream ms = new MemoryStream();
+                    using (FileStream fs = new FileStream(ImgPath, FileMode.Open))
+                    {
+                        byte[] buffer = new byte[fs.Length];
+                        fs.Read(buffer, 0, buffer.Length);
+                        ms.Write(buffer, 0, buffer.Length);
+                    }
+
+                    Bitmap bitmap = new Bitmap(ms);
+                    System.Console.WriteLine(uploadPath + "origin.png");
+                    System.Console.WriteLine(savePath + "output.png");
+                    // Dirty Code
+                    bitmap.Save(readPath + "origin.png");
+                    originImg.Load(readPath + "origin.png");
+                    bitmap.Save(readPath + "output.png");
+                    procImg.Load(readPath + "output.png");
+
+                    // Save Image
+                    bitmap.Save(uploadPath + "origin.png");
+                    bitmap.Save(savePath + "output.png");
+                    bitmap.Dispose();
                 }
-                catch( Exception ex )
+                catch (Exception ex)
                 {
-                    MessageBox.Show (ex.Message);
+                    MessageBox.Show(ex.Message);
                 }
                 originImg.Load(uploadPath + "origin.png");
                 procImg.Load(savePath + "output.png");
+
             }
         }
+
+        private void HistogramBT_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = (Bitmap)Image.FromFile(uploadPath + "origin.png");
+            int[] ImgData = new int[256];
+            LockBitmap lockBitmap = new LockBitmap(bmp);
+            lockBitmap.LockBits();
+            Color compareClr = Color.FromArgb(255, 255, 255, 255);
+            for (int y = 0; y < lockBitmap.Height; y++)
+            {
+                for (int x = 0; x < lockBitmap.Width; x++)
+                {
+                    int val = (int)((lockBitmap.GetPixel(x, y).R + lockBitmap.GetPixel(x, y).G + lockBitmap.GetPixel(x, y).B) / 3.0);
+                    ImgData[val]++;
+                }
+            }
+            lockBitmap.UnlockBits();
+            System.Console.WriteLine(lockBitmap.Height * lockBitmap.Width);
+            Bitmap Histogram = null;
+            drawHistogram(ref ImgData, 256, 300, out Histogram);
+            bmp.Dispose();
+
+            // Dirty Code
+            Histogram.Save(readPath + "Histogram.png");
+            procImg.Load(readPath + "Histogram.png");
+
+            Histogram.Save(savePath + "Histogram.png");
+            procImg.Load(savePath + "Histogram.png");
+            Histogram.Dispose();
+        }
+
+        private void Guassion_NoiseBT_Click(object sender, EventArgs e)
+        {
+            string value = "10";
+            int stdVal = 0;
+            if (InputBox.InputBoxImpement("STD", "", ref value) == DialogResult.OK)
+            {
+                stdVal = Convert.ToInt32(value);
+            }
+            Bitmap bmp = (Bitmap)Image.FromFile(uploadPath + "origin.png");
+
+            // Dirty code
+            Bitmap bmp_histogram = (Bitmap)Image.FromFile(savePath + "Guassian_Histogram.png");
+            bmp_histogram.Save(readPath + "Guassian_Histogram.png");
+            procImg.Load(readPath + "Guassian_Histogram.png");
+            bmp_histogram.Dispose();
+            Bitmap G_Bmp = AddNoise(bmp, stdVal, 0);
+
+            // Dirty Code
+            G_Bmp.Save(readPath + "Guassian_Noise.png");
+            originImg.Load(readPath + "Guassian_Noise.png");
+
+            G_Bmp.Save(savePath + "Guassian_Noise.png");
+            G_Bmp.Dispose();
+
+            originImg.Load(savePath + "Guassian_Noise.png");
+
+            procImg.Load(savePath + "Guassian_Histogram.png");
+        }
+
+
+        // Add Guassian Noise to NewBitmap and Return it.
+        public static Bitmap AddNoise(Bitmap OriginalImage, int stdDev, int mean)
+        {
+            int[] GuassianVal = new int[8 * stdDev + 1];
+            Bitmap NewBitmap = new Bitmap(OriginalImage.Width, OriginalImage.Height);
+            LockBitmap newBitmap = new LockBitmap(NewBitmap);
+            LockBitmap oldBitmap = new LockBitmap(OriginalImage);
+            newBitmap.LockBits();
+            oldBitmap.LockBits();
+            Random TempRandom = new Random();
+
+            for (int y = 0; y < newBitmap.Height; y++)
+            {
+                for (int x = 0; x < newBitmap.Width; x++)
+                {
+                    double u1 = TempRandom.NextDouble(); //these are uniform(0,1) random doubles
+                    double u2 = TempRandom.NextDouble();
+                    double randStdNormal = Math.Sqrt(-2.0 * Math.Log(u1)) *
+                                 Math.Sin(2.0 * Math.PI * u2); //random normal(0,1)
+                    int randNormal =
+                                 (int)(mean + stdDev * randStdNormal); //random normal(mean,stdDev^2)
+
+                    randNormal = randNormal > 4 * stdDev ? 4 * stdDev : randNormal;
+                    randNormal = randNormal < -4 * stdDev ? -4 * stdDev : randNormal;
+
+                    GuassianVal[randNormal + 4 * stdDev]++;
+                    int R = oldBitmap.GetPixel(x, y).R + randNormal;
+                    int G = oldBitmap.GetPixel(x, y).G + randNormal;
+                    int B = oldBitmap.GetPixel(x, y).B + randNormal;
+                    // Normalize
+                    R = R > 255 ? 255 : R;
+                    R = R < 0 ? 0 : R;
+                    G = G > 255 ? 255 : G;
+                    G = G < 0 ? 0 : G;
+                    B = B > 255 ? 255 : B;
+                    B = B < 0 ? 0 : B;
+                    // Set Guassian White Noise
+                    Color TempValue = Color.FromArgb(R, G, B);
+                    oldBitmap.SetPixel(x, y, TempValue);
+                }
+            }
+            newBitmap.UnlockBits();
+            oldBitmap.UnlockBits();
+            Bitmap CuassianNoise = null;
+            GuassianVal[GuassianVal.Length / 2] /= 2;
+            drawHistogram(ref GuassianVal, GuassianVal.Length, 300, out CuassianNoise);
+
+            CuassianNoise.Save(savePath + "Guassian_Histogram.png");
+            return OriginalImage;
+        }
+        //Draw Histogram
+        private static bool drawHistogram(ref int[] datas, int width, int height, out Bitmap myimage)
+        {
+            int WIDTH_BUF = width / 11;
+            int HEIGHT_BUF = height / 10;
+            myimage = new Bitmap(width + WIDTH_BUF * 2, height + HEIGHT_BUF);
+            Graphics g = Graphics.FromImage(myimage);
+            Pen pen = new Pen(Color.Blue);
+            int[] cpDatas = new int[datas.Length];
+            int MAX_VALUE = getMaxVal(ref datas);
+
+            Array.Copy(datas, 0, cpDatas, 0, datas.Length);
+            if (MAX_VALUE > height)
+            {
+                for (int i = 0; i < cpDatas.Length; i++)
+                {
+                    cpDatas[i] = System.Convert.ToInt32(cpDatas[i] * height / MAX_VALUE);
+                }
+            }
+            else
+            {
+                height = MAX_VALUE;
+            }
+
+            for (int i = 0; i < cpDatas.Length; i++)
+            {
+                g.DrawLine(pen, i + WIDTH_BUF, height - cpDatas[i] + height / 10, i + WIDTH_BUF, myimage.Height);
+            }
+
+            return true;
+        }
+
+        private static int getMaxVal(ref int[] datas)
+        {
+            int maxVal = 0;
+            for (int i = 0; i < datas.Length; i++)
+            {
+                if (datas[i] > maxVal) maxVal = datas[i];
+            }
+            return maxVal;
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+
     }
 }
 
